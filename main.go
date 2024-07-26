8ba3b4f8d3d266d2addd7731418e25ba3aed55d9
jmpop <pjm970128@gmail.com>
2024-07-26 23:16:40 +0900

    add - config

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -13,40 +16,49 @@ func main() {
 	// var id, password string
 	// fmt.Scan(&id, &password) // fmt.Scan 함수의 두 번째 리턴값은 생략
 	// fmt.Println(id, password)
-	err := godotenv.Load(".env")
-
+	//사용자 정보가 담겨 있는 json 파일
+	env_data, err := os.Open("config.json")
+	type Info struct {
+		Id  string
+		Pwd string
+	}
+	//에러 확인
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println(err)
 	}
 
-	fmt.Println("env [userId]:", os.Getenv("userId"))
+	//open한 json을 읽는다
+	var info Info
+	byteValue, _ := ioutil.ReadAll(env_data)
+	json.Unmarshal(byteValue, &info)
+	fmt.Println(info.Id)
 
-	// // api_login
-	// login_api := "https://accounts.interpark.com/login/submit"
-	// client := &http.Client{}
-	// bodyData := url.Values{
-	// 	"postProc": {"FULLSCREEN"},
-	// 	"LOGIN_TP": {"1500"},
-	// 	"formSVC":  {"inpark"},
-	// 	"bizId":    {"15"},
-	// 	"userId":   {""},
-	// 	"userPwd":  {""},
-	// }
+	// api_login
+	login_api := "https://accounts.interpark.com/login/submit"
+	client := &http.Client{}
+	bodyData := url.Values{
+		"postProc": {"FULLSCREEN"},
+		"LOGIN_TP": {"1500"},
+		"formSVC":  {"inpark"},
+		"bizId":    {"15"},
+		"userId":   {info.Id},
+		"userPwd":  {info.Pwd},
+	}
 
-	// req, _ := http.NewRequest("POST", login_api, bytes.NewBufferString(bodyData.Encode()))
-	// req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// defer resp.Body.Close()
+	req, _ := http.NewRequest("POST", login_api, bytes.NewBufferString(bodyData.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
 
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// data := string(body)
+	data := string(body)
 
-	// log.Println(data)
+	log.Println(data)
 }
