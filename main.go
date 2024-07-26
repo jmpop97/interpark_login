82d697d3b93b1e51bed08d30b4d39d34445d3e7f
jmpop <pjm970128@gmail.com>
2024-07-26 22:24:33 +0900

    add - input

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//input
+	var id, password string
+	fmt.Scan(&id, &password) // fmt.Scan 함수의 두 번째 리턴값은 생략
+	fmt.Println(id, password)
+
+	// api
+
+	//
+}
