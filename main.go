58623cfb2fb520a0382200d7a4bcb1c9fe981793
jmpop <pjm970128@gmail.com>
2024-08-02 13:30:32 +0900

    add get id_token

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"time"
 )
 
 type InputValue struct {
@@ -21,15 +20,16 @@ type InputValue struct {
 func main() {
 	var info InputValue
 	info = input2()
-	log_in(info)
-	// check_login()
-	// check_login_mypage(info)
-	// create_pcid()
-	create_pcid()
+	create_JSESSIONID()
+	var callback_url string
+	var JSESSIONID string
+	var id_token string
+	callback_url, JSESSIONID = get_callback_url(info)
+	id_token = login(callback_url, JSESSIONID)
+	fmt.Println("id_token = " + id_token)
 }
 
 func input1() InputValue {
-	// fmt.Println(id, password
 	var info InputValue
 	fmt.Scan(&info.Id, &info.Pwd) // fmt.Scan 함수의 두 번째 리턴값은 생략
 	return info
@@ -48,7 +48,6 @@ func input2() InputValue {
 }
 
 func check_login_lpoint() {
-	fmt.Print("work")
 	check_log_in_api := "https://www.interpark.com/api/member/point/nolpoint"
 	client := &http.Client{}
 	// bodyData := url.Values{}
@@ -60,25 +59,14 @@ func check_login_lpoint() {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	headers := resp
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	data := string(body)
-
-	log.Println(data, headers)
 }
 
 func check_login_mypage(info InputValue) {
-	fmt.Print("work")
 	check_log_in_api := "https://www.interpark.com/_next/data/CQci7lqPuZOYI4_CbXD9M/mypage.json"
 	client := &http.Client{}
 	// bodyData := url.Values{}
 	req, _ := http.NewRequest("GET", check_log_in_api, nil)
 	req.Header.Add("Cookie", "id_token="+info.IdToken+";")
-	log.Println((req))
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
@@ -95,39 +83,70 @@ func check_login_mypage(info InputValue) {
 	log.Println(data, headers)
 }
 
-func log_in(info InputValue) {
+func get_callback_url(info InputValue) (string, string) {
 	login_api := "https://accounts.interpark.com/login/submit"
 	client := &http.Client{}
 	bodyData := url.Values{
-		"postProc": {"FULLSCREEN"},
-		"LOGIN_TP": {"1500"},
-		"formSVC":  {"inpark"},
-		"bizId":    {"15"},
 		"userId":   {info.Id},
 		"userPwd":  {info.Pwd},
+		"LOGIN_TP": {"1000"},
 	}
 	req, _ := http.NewRequest("POST", login_api, bytes.NewBufferString(bodyData.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("Host", "accounts.interpark.com")
 	req.Header.Add("x-requested-with", "XMLHttpRequest")
-	log.Print(req)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	headers := resp
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	data := string(body)
-
-	log.Println(data, headers)
+	s := string(body)
+	var data map[string]interface{}
+	err2 := json.Unmarshal([]byte(s), &data)
+	if err != nil {
+		panic(err2)
+	}
+	var callback_url string
+	var JSESSIONID string
+	callback_url = data["callback_url"].(string)
+	cookies := resp.Cookies()
+	for _, cookie := range cookies {
+		if cookie.Name == "JSESSIONID" {
+			JSESSIONID = cookie.Name + "=" + cookie.Value + ";"
+			break
+		}
+	}
+	return callback_url, JSESSIONID
 }
 
-func create_pcid() {
-	var PCID = time.Now().UnixNano()
-	fmt.Println(PCID)
+func create_JSESSIONID() {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://accounts.interpark.com/login/form", nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
+
+func login(callback_url string, JSESSIONID string) string {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", callback_url, nil)
+	req.Header.Add("Cookie", JSESSIONID)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	cookies := resp.Cookies()
+	for _, cookie := range cookies {
+		if cookie.Name == "id_token" {
+			return cookie.Value
+		}
+	}
+	return "wrong id"
 }
